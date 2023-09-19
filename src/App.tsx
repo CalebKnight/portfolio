3a5a94ef137ea303f254ef308c2a7a94ba0e53a5
Caleb <caleb26502@gmail.com>
2023-09-19 19:32:46 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-slate-950 to-slate-900 w-screen h-screen">
+      <div className="flex m-auto justify-center items-center w-full h-full ">
+        <h1 className="font-bold text-3xl md:text-5xl lg:text-7xl text-zinc-100 text-center">
+          This site is under construction!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
